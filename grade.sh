b8c5907676c230d27a86424700b804b306128151
arjunbhalla03 <arjunbhalla03@gmail.com>
2023-11-27 14:11:59 -0800

    random commits

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -6,11 +6,28 @@ rm -rf grading-area
 mkdir grading-area
 
 git clone $1 student-submission
+
+if [[ -f "student-submission/ListExamples.java" ]]
+then
+mv student-submission/ListExamples.java grading-area
+mv TestListExamples.java grading-area
+mv lib grading-area
 echo 'Finished cloning'
+cd grading-area
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+
+else
+echo 'Repository cloned incorrectly. Please try again.'
+exit
+fi
 
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
+
+
 # tests
